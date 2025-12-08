6a038ea85b16e84a6702551aa6c050c534a25c06
silvergin7 <timbjornhov@gmail.com>
2025-12-08 13:33:07 +0100

    new

--- contracts/Lektion 1/HelloWorld.sol
+++ contracts/Lektion 1/HelloWorld.sol
@@ -3,5 +3,13 @@ pragma solidity 0.8.31;
 
 contract HelloWorld {
     // Ett smart kontrakt består av variabler och funktioner
+    // State variabel
     string public message = "Hello World!";
+
+    // Write funktion
+    // Vi vill kunna skicka in ett nytt meddelande, för att uppdatera message variabeln
+    // newMessage är en lokal variabel
+    function setMessage(string memory newMessage) public {
+    message = newMessage; 
+    }
 }
