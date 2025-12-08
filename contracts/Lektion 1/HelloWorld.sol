db058023078427e4f7d9dbbc1b4df661bb56c273
silvergin7 <timbjornhov@gmail.com>
2025-12-08 13:15:24 +0100

    test

---
+++ contracts/Lektion 1/HelloWorld.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.31;
+
+contract HelloWorld {
+    // Ett smart kontrakt består av variabler och funktioner
+    string public message = "Hello World!";
+}
