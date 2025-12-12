43f970f148686d8988302059c3359ca3c1231851
Tim <timbjornhov@gmail.com>
2025-12-12 10:54:47 +0100

    'Expanding on Library'

--- contracts/Lektion2/Library.sol
+++ contracts/Lektion2/Library.sol
@@ -11,7 +11,36 @@ contract Library {
     // Vi skapar en array av book struct
     Book[] public books;
 
-    function addBook(string memory bookTitle, string memory bookAuthor, uint16 bookYear) public {
+    function addBook(
+        string memory bookTitle,
+        string memory bookAuthor,
+        uint16 bookYear
+    ) public {
         books.push(Book(bookTitle, bookAuthor, bookYear));
+    }
+
+    // Funktion för att ta reda på hur många böcker vi har i biblioteket
+    function bookCount() public view returns (uint) {
+        return books.length;
+    }
+
+    function removeBook(uint index) public {
+        require(index < books.length, "This book does not exist");
+        // Delete kommer att nollställa värdena på detta index, men index ligger kvar
+        delete books[index];
+    }
+
+    function removeBookIndex(uint index) public {
+        require(index < books.length, "This book does not exist");
+        books[index] = books[books.length - 1];
+        books.pop();
+    }
+
+    function removeBookKeepOrder(uint index) public {
+        require(index < books.length, "This book does not exist");
+        for (uint i = index; i < books.length - 1; i++) {
+            books[i] = books[i + 1];
+        }
+        books.pop();
     }
 }
